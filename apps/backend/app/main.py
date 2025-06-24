f460beae1033aa77f05d211a24c19d655468f4fc
alonemanuel <alonemanuel95@gmail.com>
2025-06-24 12:22:51 +0300

    Add allowed origin for frontend deployment; include new URL for project-vista.pages.dev in CORS settings.

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
+        "https://project-vista.pages.dev/",
     ],  # Add your frontend URLs
     allow_credentials=True,
     allow_methods=["*"],
